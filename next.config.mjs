fb851c949f7192d138f8c5f4a375869365a3d4fe
Lin Lập Kiến <linlapkien@gmail.com>
2024-08-27 08:46:15 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,29 @@
+import NextFederationPlugin from '@module-federation/nextjs-mf';
+//npm install @module-federation/nextjs-mf
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'feature_component',
+        remotes: {},
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './Feature': './pages/components/feature.tsx',
+        },
+        extraOptions: {
+          debug: false, // `false` by default
+          exposePages: false, // `false` by default
+        },
+        shared: {},
+      })
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
